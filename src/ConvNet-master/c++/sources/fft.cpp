eb4eaf5839ded370e80ae80fab1199e6208640d6
mwoodson1 <markus.woodson1@gmail.com>
2015-04-26 18:20:06 -0400

    Added convolution code
    Additional to-do list added in fft.cpp for reference.

--- src/ConvNet-master/c++/sources/fft.cpp
+++ src/ConvNet-master/c++/sources/fft.cpp
@@ -11,28 +11,68 @@
       the dimensions are not powers of 2
 */
 
+//TODO
+//- Implement in IFFT
+//- Work on parallelization
+//- Get interface to starter code 
+//- All of the functions do in-place changes, we might need to change that???
+//- Run performance tests for different sized kernels on 32x32
+//- Run performance tests for different sized kernels on real sized images.
+//- Figure out how to write a program using the Xeon Phi's on latedays
+//- Parallelize different parts of the starter neural net code
+//   - We could parallelize batch learning
+//   - A lot of his functions use loops I think can be vectorized
+//   - 
+// ... misc. stuff may be added later
+
 const double PI = 3.141592653589793238460;
 
 typedef std::complex<double> Complex;
-typedef std::valarray<Complex> CArray;
+typedef std::vector<Complex> CArray;
 typedef std::vector<CArray> C2D;
 
 using namespace std;
 void FFT(CArray& x);
-//void FFT(int dir, long m, std::complex<double> *x);
+int FFT2D(C2D& x,int nx,int ny,int dir);
+void makeSize(C2D& kern,int imgW, int imgH, int kernW, int kernH);
 int Powerof2(int n,int *m,int *twopm);
 
-void conv(C2D& img, C2D& kernel,C2D& out, int imgW, int imgH, int kernW, int kernH){
-  //Take FFT2D of img and kernel
-  //Point-wise multiplications
-  //Take IFFT to get result
+void makeSize(C2D& kern,int imgW, int imgH, int kernW, int kernH){
+  //We will assume the kernel and image are square
+  int dif = imgW - kernW;
+  if(dif == 0) return;
+
+  std::cout << dif << std::endl;
+  kern.resize(imgH);
+  CArray tmp(imgW,0.0);
+  for(int row=0; row<imgH; row++){
+    kern[row].resize(imgW,0.0);
+  }
+  return;
 }
 
+void conv(C2D& img, C2D& kernel,C2D& out, int imgW, int imgH, int kernW, int kernH){
+  //Need to pad the kernel to make it the same dimension as the image
+  makeSize(kernel,imgW,imgH,kernW,kernH);
+  //Take FFT2D of img and kernel
+  FFT2D(img,imgW,imgH,1);
+  FFT2D(kernel,imgW,imgH,1);
+  //Point-wise multiplications
+  for(int i=0; i<imgH; i++){
+    for(int j=0; j<imgW; j++){
+      img[i][j] = img[i][j] * kernel[i][j];
+    }
+  }
+  //Take IFFT to get result
+  FFT2D(img,imgW,imgH,-1);
+}
+
+//Need to implement the inverse FFT and FFT2D
 int FFT2D(C2D& x,int nx,int ny,int dir)
 {
   int i,j;
 
-  CArray tmp(0.0,nx);
+  CArray tmp(nx,0.0);
   for(j=0;j<ny;j++){
     for(i=0;i<nx;i++){
       tmp[i] = x[i][j];
@@ -42,8 +82,8 @@ int FFT2D(C2D& x,int nx,int ny,int dir)
       x[i][j] = tmp[i];
     }
   }
-
-  CArray tmp2(0.0,ny);
+  
+  CArray tmp2(ny,0.0);
   for(i=0;i<nx;i++){
     for(j=0;j<ny;j++){
       tmp2[j] = x[i][j];
@@ -53,68 +93,8 @@ int FFT2D(C2D& x,int nx,int ny,int dir)
       x[i][j] = tmp2[j];
     }
   }
-   
-  /*
-   int i,j;
-   int m,twopm;
-   double *real,*imag;
-   std::complex<double> *tmp = new std::complex<double>[nx];
-
-   //* Transform the rows 
-   //real = (double *)malloc(nx * sizeof(double));
-   //imag = (double *)malloc(nx * sizeof(double));
-   //tmp  = (std::complex<double> *)malloc(nx * sizeof( std::complex<double> ));
-   //if (real == NULL || imag == NULL)
-   // return(0);
-   //if (!Powerof2(nx,&m,&twopm) || twopm != nx)
-   //return(0);
-   for (j=0;j<ny;j++) {
-      for (i=0;i<nx;i++) {
-         tmp[i] = c[i][j];
-         //real[i] = c[i][j].real;
-         //imag[i] = c[i][j].imag;
-      }
-      FFT(dir,m,tmp);
-      for (i=0;i<nx;i++) {
-         c[i][j] = tmp[i];
-         //c[i][j].real = real[i];
-         //c[i][j].imag = imag[i];
-      }
-   }
-   free(real);
-   free(imag);
-   //free(tmp);
-   delete [] tmp;
-
-   //* Transform the columns 
-   //real = (double *)malloc(ny * sizeof(double));
-   //imag = (double *)malloc(ny * sizeof(double));
-   tmp = new std::complex<double>[ny];
-   //tmp  = (std::complex<double> *)malloc(ny * sizeof( std::complex<double> ));
-   //if (real == NULL || imag == NULL)
-   // return(0);
-   //if (!Powerof2(ny,&m,&twopm) || twopm != ny)
-   // return(0);
-   for (i=0;i<nx;i++) {
-      for (j=0;j<ny;j++) {
-         tmp[i] = c[i][j];
-         //real[j] = c[i][j].real;
-         //imag[j] = c[i][j].imag;
-      }
-      FFT(dir,m,tmp);
-      for (j=0;j<ny;j++) {
-         c[i][j] = tmp[j];
-         //c[i][j].real = real[j];
-         //c[i][j].imag = imag[j];
-      }
-   }
-   //free(real);
-   //free(imag);
-   //free(tmp);
-   delete [] tmp;
-  */
-   return(1);
-   }
+  return(1);
+}
 
 /*-------------------------------------------------------------------------
    This computes an in-place complex-to-complex FFT
@@ -145,9 +125,23 @@ int FFT2D(C2D& x,int nx,int ny,int dir)
 void FFT(CArray& x){
   const size_t N = x.size();
   if(N <= 1) return;
+  
+  int t1, t2;
+  t1 = 0;
+  t2 = 0;
 
-  CArray even = x[std::slice(0,N/2,2)];
-  CArray odd = x[std::slice(1,N/2,2)];
+  CArray even(N/2,0);//x[std::slice(0,N/2,2)];
+  CArray odd(N/2,0); //x[std::slice(1,N/2,2)];
+  for(int i=0; i<N; i++){
+    if(i%2 == 0){
+      even[t1] = x[i];
+      t1++;
+    }
+    else{
+      odd[t2] = x[i];
+      t2++;
+    }
+  }
 
   FFT(even);
   FFT(odd);
@@ -158,77 +152,6 @@ void FFT(CArray& x){
     x[k+N/2] = even[k] - t;
   }
 }
-
-/*
-void FFT(int dir, long m, std::complex<double> *x)
-{
-   long i, i1, i2,j, k, l, l1, l2, n;
-   std::complex<double> tx, t1, u, c;
-
-   //*Calculate the number of points 
-   n = 1;
-   for(i = 0; i < m; i++) 
-      n <<= 1;   
-
-   //* Do the bit reversal 
-   i2 = n >> 1;
-   j = 0;
-
-   for (i = 0; i < n-1 ; i++)
-   {
-      if (i < j)
-         swap(x[i], x[j]);
-
-      k = i2;
-
-      while (k <= j) 
-     {
-         j -= k;
-         k >>= 1;
-      }
-
-      j += k;
-   }
-
-   //* Compute the FFT 
-   c.real(-1.0);
-   c.imag(0.0);
-   l2 = 1;
-   for (l = 0; l < m; l++) 
-   {
-      l1 = l2;
-      l2 <<= 1;
-      u.real(1.0);
-      u.imag(0.0);
-
-      for (j = 0; j < l1; j++) 
-     {
-         for (i = j; i < n; i += l2) 
-       {
-            i1 = i + l1;
-            t1 = u * x[i1];
-            x[i1] = x[i] - t1; 
-            x[i] += t1;
-         }
-
-         u = u * c;
-      }
-
-      c.imag(sqrt((1.0 - c.real()) / 2.0));
-      if (dir == 1)
-         c.imag(-c.imag());
-      c.real(sqrt((1.0 + c.real()) / 2.0));
-   }
-
-   //* Scaling for forward transform 
-   if (dir == 1) 
-   {
-      for (i = 0; i < n; i++)
-         x[i] /= n;      
-   }   
-   return;
-}
-*/
 
 /*-------------------------------------------------------------------------
    Calculate the closest but lower power of two of a number
@@ -258,47 +181,44 @@ int Powerof2(int n,int *m,int *twopm)
 
 int main(){
   
+  //Testing FFT
   const Complex test[] = {1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0};
-  CArray data(test,8);
+  CArray data(test,test + sizeof(test) / sizeof(Complex));
   FFT(data);
+  
   std::cout << "fft" << std::endl;
   for(int i=0; i<8; i++){
     std::cout << data[i] << std::endl;
   }
 
+  //Testing FFT2D
   const Complex test1[] = {1.0, 1.0};
-  CArray test11(test1,2);
-  std::vector<CArray> test2;
-  test2.push_back(test11);
-  test2.push_back(test11);
+  CArray row(test1,test1 + sizeof(test1) / sizeof(Complex));
+  C2D arr;
+  arr.push_back(row);
+  arr.push_back(row);
+  std::cout << arr.size() << std::endl;
+  std::cout << arr[0].size() << std::endl;
   std::cout << "2dfft" << std::endl;
-  FFT2D(test2,2,2,1);
-
+  FFT2D(arr,2,2,1);
+  
+  std::cout << arr.size() << std::endl;
+  std::cout << arr[0].size() << std::endl;
   for(int i=0; i<2; i++){
     for(int j=0; j<2; j++){
-      std::cout << test2[i][j] << std::endl;;
+      std::cout << arr[i][j] << std::endl;;
     }
   }
-  /*
-   int t;
-   int size = sizeof(std::complex<double>);
-   std::complex<double>** test = new std::complex<double>*[2];
-   for(int i=0; i<2; i++){
-     test[i] = new std::complex<double>[2];
-   }
-   test[0][0] = 1.0;
-   test[0][1] = 1.0;
-   test[1][0] = 1.0;
-   test[1][1] = 1.0;
-     //std::complex<double> test[2][2] = {{{1, 1}, {1,1}}, {{1, 1}, {1,1}}};
-   t = FFT2D(test,2,2,1);
-   // output each array element's value                      
-   for ( int i = 0; i < 2; i++ ){
-      for ( int j = 0; j < 2; j++ )
-      {
-         cout << "a[" << i << "][" << j << "]: ";
-         cout << test[i][j]<< endl;
-      }
-      }*/
+
+  //Testing resize
+   makeSize(arr,8,8,2,2);
+
+  for(int i=0; i<8; i++){
+    for(int j=0; j<8; j++){
+      std::cout << arr[i][j];
+    }
+    std::cout << std::endl;
+  }
+  
    return 0;
 }
